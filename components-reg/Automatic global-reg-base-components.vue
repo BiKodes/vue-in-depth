dc1cc9eab0e62fb27928cb91c53dd16c865b8219
BikoPython <stevennyandoya@gmail.com>
2021-05-03 15:09:08 +0300

    component-registration

---
+++ components-reg/Automatic global-reg-base-components.vue
@@ -0,0 +1,31 @@
+<template>
+
+<BaseInput
+  v-model="searchText"
+  @keydown.enter="search"/>
+
+<BaseButton @click="search">
+  <BaseIcon name="search"/>
+</BaseButton>
+
+</template>
+
+<script>
+
+import BaseButton from './BaseButton.vue'
+import BaseIcon from './BaseIcon.vue'
+import BaseInput from './BaseInput.vue'
+
+export default {
+    components: {
+        BaseButton,
+        BaseIcon,
+        BaseInput
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components-reg/local-reg-mdule-systems.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+import ComponentA from './ComponentA'
+import ComponentC from './ComponentC'
+
+
+export default {
+    components: {
+        ComponentA,
+        ComponentC
+    },
+
+}
+</script>
+
+<style>
+
+</style>
